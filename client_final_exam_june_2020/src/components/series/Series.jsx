2a6c0180f8812329ec4a63c1ea1d99237b803af5
odeane_croney <odeane_croney@yahoo.com>
2020-09-09 15:12:51 +0200

    mapping through images and displaying them

--- client_final_exam_june_2020/src/components/series/Series.jsx
+++ client_final_exam_june_2020/src/components/series/Series.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function Series() {
 
+  const [series, setSeries] = useState([])
+
+  useEffect(() => {
+   getSeries()
+  }, [])
+
+  const getSeries = async () => {
+    let response = await axios.get('https://content.viaplay.se/pc-se/serier/samtliga')
+    setSeries(response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products'])
+  }
+
+  let seriesImages = (
+    series.map(serie =>
+      <img src={serie.content.images.boxart.url} alt="" className='series__img'/>
+    )
+  )
 
   return (
-    <div>
-      
+    <div className="series_container">
+      {seriesImages}
     </div>
   )
 }
